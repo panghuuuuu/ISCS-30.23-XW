7ddcc249a9442039486ac81e21822aa5662973f0
Ysabella Panghulan <95083718+panghuuuuu@users.noreply.github.com>
2024-11-17 17:21:34 +0800

    created Dockerfile for backend

--- orsem-django/orsem/urls.py
+++ orsem-django/orsem/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
 	path('', include('adminposts.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path( 'blockinfo/', include( 'blockinfo.urls' ) ),
     path('auth/', include('authentication.urls')),
     path('concernportal/', include('concernportal.urls')),
